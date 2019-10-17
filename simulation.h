f5f438de5d13bf2e48dba97322f4b6312c602c64
Harish238 <harishvenkat238@gmail.com>
2019-10-16 21:41:28 -0500

    Added boilerplate function definitions

--- simulation.h
+++ simulation.h
@@ -1,9 +1,18 @@
+#include <GL/glut.h>
+
 #ifndef _SIMULATION_H_
 #define _SIMULATION_H_
 #endif
 
+// Drawing functions
 void drawHorse();
 void drawMerryGoRound();
 void drawCar();
 void drawWheel();
 void drawPlane();
+
+// Boiler-plate functions
+void init();
+void display();
+void reshape(GLint, GLint);
+GLvoid keyboard(unsigned char, int, int);
