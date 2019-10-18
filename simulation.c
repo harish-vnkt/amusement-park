d52e03f345bd5b93f4978e496b181c71797d72f2
Harish238 <harishvenkat238@gmail.com>
2019-10-18 18:30:01 -0500

    Positioned directional light source and merry-go-round objects

--- simulation.c
+++ simulation.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
     glutInitWindowSize(width, height);
     glutCreateWindow("Amusement Park");
 
-    glClearColor( 0.0, 0.0, 0.0, 1.0 );
+    glClearColor( 47.055, 55.289, 86.785, 1.0 ); // sky blue
 
     glutReshapeFunc(reshape);
     glutDisplayFunc(render);
@@ -27,12 +27,23 @@ int main(int argc, char** argv) {
 
 void init() {
 
+    GLfloat direction[] = {1.0f, 1.0f, 1.0f, 0.0f};
+    GLfloat white[] = {1.0f, 1.0f, 1.0f, 1.0f};
+
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+	glLightfv(GL_LIGHT0, GL_POSITION, direction);
+
+	glEnable(GL_LIGHTING);                
+	glEnable(GL_LIGHT0);
+    
+    glColorMaterial(GL_FRONT, GL_DIFFUSE);
+
+    glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_DEPTH_TEST);
 
     glEnable(GL_LINE_SMOOTH);
     glHint(GL_LINE_SMOOTH_HINT, GL_NICEST); // Antialias lines
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 }
 
@@ -46,7 +57,7 @@ void reshape(GLint w, GLint h) {
     glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(60, (float)w / (float)h, 1.0f, 120.0f);
-	gluLookAt(0.0f, 10.0f, 15.0f,
+	gluLookAt(0.0f, 30.0f, 60.0f,
 		0.0f, 0.0f, 0.0f,   
 		0.0f, 1.0f, 0.0f);  
 
@@ -66,6 +77,53 @@ GLvoid keyboard(unsigned char key, int x, int y) {
 void render() {
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+        
+        drawPlane();
+
+        glPushMatrix();
+
+            glTranslatef(0.0f, 0.0f, -25.0f);
+            glScalef(0.7f, 0.7f, 0.7f);
+            drawMerryGoRound();
+
+            glPushMatrix();
+
+                glTranslatef(15.0f, 10.0f, 0.0f);
+                glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
+                drawHorse();
+
+            glPopMatrix();
+
+            glPushMatrix();
+
+                glTranslatef(0.0f, 10.0f, 15.0f);
+                glRotatef(0.0f, 0.0f, 1.0f, 0.0f);
+                drawHorse();
+
+            glPopMatrix();
+
+            glPushMatrix();
+
+                glTranslatef(-15.0f, 10.f, 0.0f);
+                glRotatef(-90.0f, 0.0f, 1.0f, 0.0f);
+                drawHorse();
+
+            glPopMatrix();
+
+            glPushMatrix();
+
+                glTranslatef(0.0f, 10.f, -15.0f);
+                glRotatef(180.0f, 0.0f, 1.0f, 0.0f);
+                drawHorse();
+
+            glPopMatrix();
+
+        glPopMatrix();
+
+    glPopMatrix();
+
     glutSwapBuffers();
 
 }
