3d5e17210718f32e79a2df682a4e10eaa2f3887a
Harish238 <harishvenkat238@gmail.com>
2019-10-19 00:36:23 -0500

    Finished merry-go-round animation

--- simulation.c
+++ simulation.c
@@ -1,12 +1,22 @@
 #include "simulation.h"
 #include <stdio.h>
 #include <GL/glut.h>
+#include <math.h>
+
+#define PI 3.14159265
 
 GLint width = 1100;
 GLint height = 650;
 
 GLfloat rotateMerryGoRound = 0.0f;
 GLfloat speedMerryGoRound = 0.75f;
+
+GLfloat rotateHorse = 0.0f;
+GLfloat speedHorse = 2.0f;
+
+GLfloat minHorseY = 5.0f;
+GLfloat maxHorseY = 17.0f;
+GLfloat rangeHorseY, horseY1, horseY2, horseY3, horseY4;
 
 int main(int argc, char** argv) {
 
@@ -30,6 +40,12 @@ int main(int argc, char** argv) {
 }
 
 void init() {
+    
+    rangeHorseY = maxHorseY - minHorseY;
+    horseY1 = 0.0f;
+    horseY2 = 45.0;
+    horseY3 = 90.0;
+    horseY4 = 135.0;
 
     GLfloat direction[] = {1.0f, 1.0f, 1.0f, 0.0f};
     GLfloat white[] = {1.0f, 1.0f, 1.0f, 1.0f};
@@ -61,7 +77,7 @@ void reshape(GLint w, GLint h) {
     glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(60, (float)w / (float)h, 1.0f, 120.0f);
-	gluLookAt(0.0f, 0.0f, 60.0f,
+	gluLookAt(0.0f, 30.0f, 60.0f,
 		0.0f, 0.0f, 0.0f,   
 		0.0f, 1.0f, 0.0f);  
 
@@ -72,10 +88,19 @@ void reshape(GLint w, GLint h) {
 void idle() {
 
     rotateMerryGoRound += speedMerryGoRound;
+    horseY1 += speedHorse;
+    horseY2 += speedHorse;
+    horseY3 += speedHorse;
+    horseY4 += speedHorse;
     
     if (rotateMerryGoRound==360.0) {
         rotateMerryGoRound = 0.0f;
     }
+
+    if (horseY1>=180.0) horseY1 = 0.0f;
+    if (horseY2>=180.0) horseY2 = 0.0f;
+    if (horseY3>=180.0) horseY3 = 0.0f;
+    if (horseY4>=180.0) horseY4 = 0.0f;
 
     glutPostRedisplay();
 
@@ -107,7 +132,9 @@ void render() {
 
             glPushMatrix();
 
-                glTranslatef(15.0f, 10.0f, 0.0f);
+                glTranslatef(15.0f, 
+                    minHorseY + (sin(horseY1 * (PI/180.0)) * rangeHorseY), 
+                        0.0f);
                 glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
                 drawHorse();
 
@@ -115,7 +142,9 @@ void render() {
 
             glPushMatrix();
 
-                glTranslatef(0.0f, 10.0f, 15.0f);
+                glTranslatef(0.0f, 
+                    minHorseY + (sin(horseY2 * (PI/180.0)) * rangeHorseY), 
+                        15.0f);
                 glRotatef(0.0f, 0.0f, 1.0f, 0.0f);
                 drawHorse();
 
@@ -123,7 +152,9 @@ void render() {
 
             glPushMatrix();
 
-                glTranslatef(-15.0f, 10.0f, 0.0f);
+                glTranslatef(-15.0f, 
+                    minHorseY + (sin(horseY3 * (PI/180.0)) * rangeHorseY), 
+                        0.0f);
                 glRotatef(-90.0f, 0.0f, 1.0f, 0.0f);
                 drawHorse();
 
@@ -131,7 +162,9 @@ void render() {
 
             glPushMatrix();
 
-                glTranslatef(0.0f, 10.0f, -15.0f);
+                glTranslatef(0.0f, 
+                    minHorseY + (sin(horseY4 * (PI/180.0)) * rangeHorseY), 
+                        -15.0f);
                 glRotatef(180.0f, 0.0f, 1.0f, 0.0f);
                 drawHorse();
 
