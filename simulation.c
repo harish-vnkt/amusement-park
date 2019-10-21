d7ca737c18ea01a2669a815b6c9157be90c9a01b
Harish238 <harishvenkat238@gmail.com>
2019-10-21 05:52:27 -0500

    Disabled color material

--- simulation.c
+++ simulation.c
@@ -86,10 +86,7 @@ void init() {
 	glEnable(GL_LIGHTING);                
 	glEnable(GL_LIGHT0);
     glEnable(GL_LIGHT1);
-    
-    glColorMaterial(GL_FRONT, GL_DIFFUSE);
 
-    glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_DEPTH_TEST);
 
     glEnable(GL_LINE_SMOOTH);
